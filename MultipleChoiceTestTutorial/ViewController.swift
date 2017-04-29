d43c823d683ed52a0f67fbb771b7bee3f2665e40
Maria Susana Moline Venanzi <rafadiver@gmail.com>
2017-04-29 10:38:49 -0500

    1st commit

---
+++ MultipleChoiceTestTutorial/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  MultipleChoiceTestTutorial
+//
+//  Created by Maria Susana Moline Venanzi on 4/29/17.
+//  Copyright © 2017 Uberbliss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var lblQuestionLABEL: UILabel!
+    @IBOutlet weak var lblChoice1LABEL: UIButton!
+    @IBOutlet weak var lblChoice2LABEL: UIButton!
+    @IBOutlet weak var lblChoice3LABEL: UIButton!
+    @IBOutlet weak var lblChoice4LABEL: UIButton!
+    @IBOutlet weak var lblCorrectOrWrongLABEL: UILabel!
+    @IBOutlet weak var lblResetLABEL: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func pressButton1(_ sender: UIButton) {
+    }
+
+    @IBAction func pressButton2(_ sender: UIButton) {
+    }
+    @IBAction func prssButton3(_ sender: UIButton) {
+    }
+    
+    @IBAction func pressButton4(_ sender: UIButton) {
+    }
+    @IBAction func pressRessetButton(_ sender: UIButton) {
+    }
+    
+    
+}
+
